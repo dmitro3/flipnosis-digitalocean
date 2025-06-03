bb43cc3338d6c24c0987cf4ada0bd7accc5daff8
AlphaSocial <alphabananasemail@gmail.com>
2025-06-03 18:03:35 +0100

    Fix deployment configuration and build process

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -140,9 +140,26 @@ const Header = () => {
         </CreateButton>
         
         <WalletSection>
-          <WalletButton onClick={handleConnect} disabled={loading}>
-            {loading ? 'Connecting...' : 'Connect Wallet'}
-          </WalletButton>
+          {isConnected && address ? (
+            <>
+              <WalletInfo>
+                <ChainIndicator>
+                  <ChainDot chain={chain} />
+                  <span>{chains[chain]?.name || 'Unknown'}</span>
+                </ChainIndicator>
+                <WalletAddress>
+                  {address.slice(0, 6)}...{address.slice(-4)}
+                </WalletAddress>
+              </WalletInfo>
+              <WalletButton onClick={disconnectWallet}>
+                Disconnect
+              </WalletButton>
+            </>
+          ) : (
+            <WalletButton onClick={handleConnect} disabled={loading}>
+              {loading ? 'Connecting...' : 'Connect Wallet'}
+            </WalletButton>
+          )}
         </WalletSection>
       </HeaderContainer>
     </ThemeProvider>
