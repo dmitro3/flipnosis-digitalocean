382876fec54b613bc927b6cdd4a08f532730e39d
AlphaSocial <alphabananasemail@gmail.com>
2025-07-23 18:57:40 +0100

    Fix deployment configuration and build process

--- dist/assets/cursor-BUQJYldZ.js
+++
@@ -1,3 +0,0 @@
-import{s as o}from"./core-ByNLCht0.js";import"./index-_5SpP6bM.js";import"./events-CUWSvfwA.js";import"./index.es-BWGPXXzD.js";const p=o` <svg fill="none" viewBox="0 0 13 4">
-  <path fill="currentColor" d="M.5 0h12L8.9 3.13a3.76 3.76 0 0 1-4.8 0L.5 0Z" />
-</svg>`;export{p as cursorSvg};
--- dist/assets/external-link-BoBkw1WI.js
+++
@@ -1,8 +0,0 @@
-import{s as l}from"./core-ByNLCht0.js";import"./index-_5SpP6bM.js";import"./events-CUWSvfwA.js";import"./index.es-BWGPXXzD.js";const t=l`<svg fill="none" viewBox="0 0 14 15">
-  <path
-    fill="currentColor"
-    fill-rule="evenodd"
-    d="M3.74 3.99a1 1 0 0 1 1-1H11a1 1 0 0 1 1 1v6.26a1 1 0 0 1-2 0V6.4l-6.3 6.3a1 1 0 0 1-1.4-1.42l6.29-6.3H4.74a1 1 0 0 1-1-1Z"
-    clip-rule="evenodd"
-  />
-</svg>`;export{t as externalLinkSvg};
