a4f9f75de0df69b460fa20f5a54ba28d04c30dea
AlphaSocial <your-email@example.com>
2025-08-17 14:13:42 +0100

    Force add rebuilt dist directory with correct asset hashes to fix MIME type issue

---
+++ dist/assets/all-wallets-b861b884.js
@@ -0,0 +1,6 @@
+import{s as o}from"./core-085364e8.js";import"./wagmi-a145a347.js";import"./vendor-953f4813.js";import"./events-92540323.js";import"./index.es-dc7e6bd4.js";const s=o`<svg fill="none" viewBox="0 0 24 24">
+  <path
+    style="fill: var(--wui-color-accent-100);"
+    d="M10.2 6.6a3.6 3.6 0 1 1-7.2 0 3.6 3.6 0 0 1 7.2 0ZM21 6.6a3.6 3.6 0 1 1-7.2 0 3.6 3.6 0 0 1 7.2 0ZM10.2 17.4a3.6 3.6 0 1 1-7.2 0 3.6 3.6 0 0 1 7.2 0ZM21 17.4a3.6 3.6 0 1 1-7.2 0 3.6 3.6 0 0 1 7.2 0Z"
+  />
+</svg>`;export{s as allWalletsSvg};
---
+++ dist/assets/en_US-RFN65H63-bd879255.js
@@ -0,0 +1,1 @@
+import{e as t}from"./wagmi-a145a347.js";import"./vendor-953f4813.js";export{t as default};
