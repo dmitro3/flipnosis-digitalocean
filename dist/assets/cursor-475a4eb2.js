41c0391972cc5158aaf60c0c310df51c33e1b007
AlphaSocial <your-email@example.com>
2025-08-19 19:57:48 +0100

    Fresh build with offers fixes: accept buttons, player names, and removed crypto offer text

--- dist/assets/cursor-475a4eb2.js
+++
@@ -1,3 +0,0 @@
-import{s as o}from"./core-e2791404.js";import"./wagmi-bca430ae.js";import"./vendor-953f4813.js";import"./events-92540323.js";import"./index.es-8549267c.js";const m=o` <svg fill="none" viewBox="0 0 13 4">
-  <path fill="currentColor" d="M.5 0h12L8.9 3.13a3.76 3.76 0 0 1-4.8 0L.5 0Z" />
-</svg>`;export{m as cursorSvg};
--- dist/assets/en_US-RFN65H63-2f379929.js
+++
@@ -1,1 +0,0 @@
-import{e as t}from"./wagmi-bca430ae.js";import"./vendor-953f4813.js";export{t as default};
