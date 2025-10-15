7f18c21edcb81a15e80cc6578152182764e54163
AlphaSocial <your-email@example.com>
2025-10-15 12:51:54 +0100

    Direct deployment to Hetzner

--- src/components/fliptubes/index.js
+++
@@ -1,3 +0,0 @@
-export { default as FlipTubeGame } from './FlipTubeGame'
-export { default as FlipTubeGamePage } from './FlipTubeGamePage'
-export { default as FlipTubeGameErrorBoundary } from './FlipTubeGameErrorBoundary'
--- src/components/tubes/index.js
+++
@@ -1,3 +0,0 @@
-export { default as GlassTubeGame } from './GlassTubeGame'
-export { default as TubeGamePage } from './TubeGamePage'
-export { default as TubeGameErrorBoundary } from './TubeGameErrorBoundary'
